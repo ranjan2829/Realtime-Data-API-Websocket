6946a6274c735a58a15dcad6a23dfc6196e9e929
Ranjan <73215366+ranjan2829@users.noreply.github.com>
2024-10-13 10:42:16 +0000

    done

--- build/lib/Life/utils/commons.py
+++ build/lib/Life/utils/commons.py
@@ -1,9 +1,7 @@
 import os
-import sys
-
 from box.exceptions import BoxValueError
 import yaml
-from src.Life import logger
+from Life import logger
 import json
 import joblib
 from ensure import ensure_annotations
@@ -15,30 +13,28 @@ import base64
 
 
 @ensure_annotations
-
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
-       """reads yaml file and returns
+    """reads yaml file and returns
 
-       Args:
-           path_to_yaml (str): path like input
+    Args:
+        path_to_yaml (str): path like input
 
-       Raises:
-           ValueError: if yaml file is empty
-           e: empty file
+    Raises:
+        ValueError: if yaml file is empty
+        e: empty file
 
-       Returns:
-           ConfigBox: ConfigBox type
-       """
-       try:
-           with open(path_to_yaml) as yaml_file:
-               content = yaml.safe_load(yaml_file)
-               logger.info(f"yaml file: {path_to_yaml} loaded successfully")
-               logger.info(f"Loaded content: {content}")  # Add this line
-               return ConfigBox(content)
-       except BoxValueError:
-           raise ValueError("yaml file is empty")
-       except Exception as e:
-           raise e
+    Returns:
+        ConfigBox: ConfigBox type
+    """
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        raise e
     
 
 
