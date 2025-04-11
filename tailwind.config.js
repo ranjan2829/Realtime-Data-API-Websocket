a2a18965ecca677efe4d9b592057feb7b3916534
Man of steel <73215366+ranjan2829@users.noreply.github.com>
2025-04-11 18:21:06 +0000

    done

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-// tailwind.config.js
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./src/public/**/*.{html,ts,tsx}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
