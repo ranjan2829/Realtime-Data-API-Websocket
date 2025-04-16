c88d16960ed39de96d444fc5a4604d67618fc68d
ranjan2829 <ranjan.shitole3129@gmail.com>
2025-04-16 11:09:33 +0530

    Tick by Tick data

--- Fyers-Websocket/Fyers-Login.py
+++ Fyers-Websocket/Fyers-Login.py
@@ -1,7 +1,7 @@
 from fyers_apiv3 import fyersModel
 
-client_id = ""
-secret_key = ""
+client_id = "QGP6MO6UJQ-100"
+secret_key = "2X22I3160A"
 redirect_uri = "https://trade.fyers.in/api-login/redirect-uri/index.html"
 response_type = "code"  
 state = "sample_state"
